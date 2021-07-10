fcaa150fdfc9a145b0b34c4aadf81a151a2d0bef
Casper-pixel <85922524+Casper-pixel@users.noreply.github.com>
2021-07-10 18:39:43 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -4,22 +4,19 @@
 
 class Translation(object):
     
-    START_TEXT = """<b>Hey {}!!</b>
-<i>Am Just A Advance Auto Filter Bot....😉
+    START_TEXT = """<b>Hey {}Welcome to my bot!!</b>
+<i>Am Just A Advance Auto Filter Bot.... Developed by ©@$P€®😉
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+You can't add me to UR group😌😌
 
-For More Details Click Help Button Below..
-@CrazyBotsz
+
+
 </i>"""    
     
-    HELP_TEXT = """
-<b><i><u>How To Use Me!?</u></i></b>
+     """
+<b><i><u></u></i></b>
 
-<i>
--> Add Me To Any Group And Make Me Admin
--> Add Me To Your Desired Channel
-</i>
+
 
 <b>Bot Commands (Works Only In Groups) :</b>
 
@@ -48,11 +45,11 @@ For More Details Click Help Button Below..
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href=>Casper</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://github.com/Casper-pixel/Caspeebot">Click Me</a></i>
 """
